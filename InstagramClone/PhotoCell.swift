14e3d1842735c302102058671e8b48ceefa467d8
Franky Liang <frankyliang@Frankys-Mac.local>
2016-01-15 09:48:35 -0500

    first commit

---
+++ InstagramClone/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  InstagramClone
+//
+//  Created by Franky Liang on 1/14/16.
+//  Copyright © 2016 Franky Liang. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
